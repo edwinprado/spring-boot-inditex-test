d70a48c984ff4ee8b2939f3ef05b8fcc3fb86878
johnpradoavoristravelcom <john.prado@avoristravel.com>
2023-08-28 10:55:14 +0200

    spring-boot-inditex-test
    Tests implementation

--- src/test/java/com/test/indetex/service/PriceServiceTest.java
+++ src/test/java/com/test/indetex/service/PriceServiceTest.java
@@ -12,10 +12,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class PriceServiceTest {
 
+    private static final String PRODUCT_ID = "35455";
+    private static final String BRAND_ID = "1";
+
     @Autowired
     PriceService priceService;
-    String PRODUCT_ID = "35455";
-    String BRAND_ID = "1";
 
     @Test
     void on14at10ShouldGet35_50Test() {
@@ -31,23 +32,52 @@ public class PriceServiceTest {
 
     @Test
     void on14at16ShouldGet25_45Test() {
+        final Double expected = 25.45;
+        Double actual = 0d;
+        final LocalDateTime applicationDate = LocalDateTime.of(2020,6,14,16,0);
+        PriceResponseDTO priceResponse = priceService.getPriceResponse(applicationDate,PRODUCT_ID,BRAND_ID);
+        if (priceResponse != null) {
+            actual = priceResponse.getPrice();
+        }
+        assertEquals(expected,actual);
 
     }
 
     @Test
     void on14at21ShouldGet35_50Test() {
+        final Double expected = 35.50;
+        Double actual = 0d;
+        final LocalDateTime applicationDate = LocalDateTime.of(2020,6,14,21,0);
+        PriceResponseDTO priceResponse = priceService.getPriceResponse(applicationDate,PRODUCT_ID,BRAND_ID);
+        if (priceResponse != null) {
+            actual = priceResponse.getPrice();
+        }
+        assertEquals(expected,actual);
 
     }
 
     @Test
     void on15at10ShouldGet30_50Test() {
+        final Double expected = 30.50;
+        Double actual = 0d;
+        final LocalDateTime applicationDate = LocalDateTime.of(2020,6,15,10,0);
+        PriceResponseDTO priceResponse = priceService.getPriceResponse(applicationDate,PRODUCT_ID,BRAND_ID);
+        if (priceResponse != null) {
+            actual = priceResponse.getPrice();
+        }
+        assertEquals(expected,actual);
 
     }
 
     @Test
     void on15at21ShouldGet38_95Test() {
-
+        final Double expected = 38.95;
+        Double actual = 0d;
+        final LocalDateTime applicationDate = LocalDateTime.of(2020,6,15,21,0);
+        PriceResponseDTO priceResponse = priceService.getPriceResponse(applicationDate,PRODUCT_ID,BRAND_ID);
+        if (priceResponse != null) {
+            actual = priceResponse.getPrice();
+        }
+        assertEquals(expected,actual);
     }
-
-
 }
